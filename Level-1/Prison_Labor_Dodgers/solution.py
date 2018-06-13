211e315e2da8e88cdc4a84a69b78bdbac3774f0f
Valentyn Ponomarenko <bootloader@list.ru>
2018-06-12 23:12:06 -0500

    1. Level 1 - Prison Labor Dodgers challenge

---
+++ Level-1/Prison_Labor_Dodgers/solution.py
@@ -0,0 +1,11 @@
+# def​ ​answer(x,​ ​y):
+# ​ ​​ ​​ ​​ ​#​ ​your​ ​code​ ​here
+#     xy = {}
+#     for i in x:
+#         xy[i] = xy.get(i, 0) + 1
+#     for i in y:
+#         xy[i] = xy.get(i, 0) - 1
+#     for key in xy:
+#         if(xy[key] == 1 or xy[key] == -1):
+#             return key
+#     return None
