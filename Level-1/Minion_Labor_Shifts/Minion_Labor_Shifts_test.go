b462cd5e5d89e54f1b966dbdf9f655300bbf63d5
Valentyn Ponomarenko <bootloader@list.ru>
2018-06-15 15:20:03 -0500

    1. Level-1: Refactoring Minor Labor Shift

--- Level-1/Minion_Labor_Shifts/Minion_Labor_Shifts_test.go
+++ Level-1/Minion_Labor_Shifts/Minion_Labor_Shifts_test.go
@@ -5,43 +5,7 @@ import (
 	"fmt"
 )
 
-func answer(data []int, n int) []int {
-	i := 0
-
-	for i < len(data) {
-
-		count := 0
-		for j := 0; j < len(data); j++ {
-			if data[j] == data[i] {
-				count++
-			}
-		}
-
-		var newValues []int
-
-		for j := 0; j < len(data); j++ {
-			if data[j] != data[i] {
-				newValues = append(newValues, data[j])
-				//track++
-			}
-
-			if count <= n && j == i {
-				newValues = append(newValues, data[j])
-			}
-		}
-
-		data = []int(newValues)
-
-		if count <= n {
-			i++
-		}
-
-	}
-
-	return data
-}
-
-func Test_Input(t *testing.T) {
+func Test_Minion_Labor_Shifts(t *testing.T) {
 
 	testDatas := []struct {
 		data []int
